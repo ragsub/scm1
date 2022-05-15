a38f794bb5e2091710671d21f6451e6fca6abf90
ragsub <raghavsk@CPX-HQNKIR2OM19.dir.svc.accenture.com>
2022-05-15 20:29:31 +0530

    changes to settings

--- SCM/settings.py
+++ SCM/settings.py
@@ -128,7 +128,7 @@ if DEVELOPMENT_MODE is True:
         }
     }
 else:
-    DATABASES = {}
+    DATABASES = {'default':{}}
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
@@ -167,7 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
